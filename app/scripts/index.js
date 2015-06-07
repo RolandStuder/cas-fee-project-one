e2bd818f93d26af3489a93def9164c9291eef97c
lubla <lubla@users.noreply.github.com>
2015-06-07 13:47:16 +0200

    index: Completed check box added, store current notes order in the local storage.

--- app/scripts/index.js
+++ app/scripts/index.js
@@ -1,48 +1,69 @@
-function renderList(orderBy) {
-    if (!orderBy) orderBy = 'importance';
+function renderList() {
+    var settings = notesSettings.readSettings();
+
+
     var noteStorage = noteData.noteStorageSingleton.getInstance();
     var notes = noteStorage.readNotes();
-    notes = orderListBy(notes, orderBy);
+    notes = orderNotesBy(notes, settings.orderBy);
     $(getMainElement()).empty().html(noteListTemplate(notes));
+
+    // Note completed checkbox event handling.
+    $('.completed-checkbox').change(function(event) {
+
+        // Update the note's completed property and store
+        // it in the note storage.
+        var checkBox = $(event.currentTarget)[0];
+        var noteElement =  $(checkBox).parents('.note')[0];
+        var id = Number($(noteElement).attr('id'));
+        var note = noteStorage.getNote(id);
+        note.completed = checkBox.checked;
+        noteStorage.updateNote(note);
+    });
+
 }
 
 // execute on load
 var noteListTemplate = Handlebars.compile(document.getElementById('noteListTemplate').innerHTML);
 
 
-function orderListBy(list, orderBy) {
-    if (orderBy === orderByDue) {
-        list.sort(function(note1, note2) {
+function orderNotesBy(notes, orderBy) {
+    if (orderBy === notesSettings.Settings.orderByDue) {
+        notes.sort(function(note1, note2) {
             // Descending.
             return note2.due - note1.due;
         })
     }
-    else if(renderList.orderBy == orderByImportance) {
+    else if(orderBy === notesSettings.Settings.orderByImportance) {
         // Descending.
         notes.sort(function(note1, note2) {return note2.importance - note1.importance})
     }
-    return list;
+    return notes;
 }
 
 function getMainElement() {
     return document.getElementsByTagName('main')[0];
 }
 
-
 document.getElementById("new-note").onclick = function() {
     window.location.replace('editNote.html')
 };
 
+function setNotesOrderBy(orderBy) {
+    var settings = notesSettings.readSettings();
+    settings.orderBy = orderBy;
+    notesSettings.updateSettings(settings);
+    renderList();
+
+}
+
 document.getElementById("order-by-due").onclick = function() {
-    renderList(orderByDue);
+    setNotesOrderBy(notesSettings.Settings.orderByDue);
 };
 
 document.getElementById("order-by-importance").onclick = function() {
-    renderList(orderByImportance);
+    setNotesOrderBy(notesSettings.Settings.orderByImportance);
 };
-
-var orderByImportance = 'importance';
-var orderByDue = 'due';
 
 // execute on load
 renderList();
+
---
+++ app/scripts/notesSettings.js
@@ -0,0 +1,61 @@
+/**
+ * Created by Luzius on 07.06.2015.
+ */
+
+
+/**
+ *  Notes settings module. Exposes functions to read and write the notes app settings and implicitly the Settings class.
+  * @type {Function}
+ */
+var notesSettings = (function() {
+
+    /**
+     *
+     * @param {string} orderBy The order by predicate for the notes in the main page. Use one of the Settings.orderBy.. predefined values.
+     * @constructor
+     */
+    function Settings(orderBy){
+        this.orderBy = orderBy || Settings.orderByImportance;
+    }
+
+    Settings.prototype.constructor = Settings;
+
+    /**
+     * Predefined order by value
+     * @type {string} Order by importance value.
+     */
+    Settings.orderByImportance = 'importance';
+
+    /**
+     * Predefined order by value.
+     * @type {string} Order by due
+     */
+    Settings.orderByDue = 'due';
+
+    var settingsKey = 'notesSettings';
+
+    function updateSettings(settings) {
+        var settingsString = JSON.stringify(settings);
+        localStorage.setItem(settingsKey, settingsString);
+    }
+
+
+    function readSettings() {
+        var settingsString = localStorage.getItem(settingsKey);
+        if(!settingsString) {
+            return new Settings();
+        }
+        else {
+            var settingsObject = JSON.parse(settingsString)
+            return new Settings(settingsObject.orderBy);
+        }
+    }
+
+    return {
+        readSettings : readSettings,
+        updateSettings : updateSettings,
+        Settings : Settings
+    }
+
+
+}());
