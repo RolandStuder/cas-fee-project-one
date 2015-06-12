5327078a6fd7e27efc186a5b57623d6d3326715d
Roland Studer <roland.studer@gmail.com>
2015-06-12 18:09:26 +0200

    localstorage emulation module for server

---
+++ server/keyValueStorage.js
@@ -0,0 +1,48 @@
+/**
+ * Async KeyValueStorage
+ *
+ */
+
+fs = require('fs');
+
+function Storage(name) {
+
+    // intialize
+    var dbFile = createDbFile(name);
+
+    function readValue(key) { // returns an string
+        var data = fs.readFileSync(dbFile,'utf-8');
+        return JSON.parse(data)[key];
+    }
+
+    function writeValue(key, value){ // returns a string all newValue
+        var data = fs.readFileSync(dbFile,'utf-8');
+        var obj = JSON.parse(data);
+        obj[key] = value || '';
+        data = JSON.stringify(obj);
+        fs.writeFileSync(dbFile,data);
+    }
+
+    return {
+
+        getItem: readValue,
+        setItem: writeValue
+    }
+}
+
+
+function createDbFile(name) {
+    if (!(fs.existsSync('db'))) {
+        fs.mkdirSync('db');
+    }
+
+    var dbPath = "db/" + name + ".db";
+
+    if (!(fs.existsSync(dbPath))) {
+        fs.writeFileSync(dbPath,'{}')
+    }
+
+    return dbPath;
+}
+
+module.exports = Storage;
