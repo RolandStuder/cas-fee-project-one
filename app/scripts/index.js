d5bad95382e61b0444a39d2c38937a235a073d80
Roland Studer <roland.studer@gmail.com>
2015-06-13 19:17:24 +0200

    displaying state of header buttons

--- app/scripts/index.js
+++ app/scripts/index.js
@@ -1,33 +1,37 @@
-function renderList() {
-    var settings = noteSettings.readSettings();
+var template = {
+    render: function () {
+        var settings = noteSettings.readSettings();
+        if (settings.orderBy === 'due') {settings.due = true} else { settings.due = false };
+        if (settings.orderBy === 'importance') {settings.importance = true} else { settings.importance = false };
 
+        var noteStorage = noteData.noteStorageSingleton.getInstance();
+        var notes = noteStorage.readNotes(function(notes) {
+            notes = orderAndFilterNotes(notes, settings);
+            $('header').empty().html(indexHeaderTemplate(settings));
+            $(getMainElement()).empty().html(noteListTemplate(notes));
 
-    var noteStorage = noteData.noteStorageSingleton.getInstance();
-    var notes = noteStorage.readNotes(function(notes) {
-        notes = orderAndFilterNotes(notes, settings);
-        $(getMainElement()).empty().html(noteListTemplate(notes));
+            // Note completed checkbox event handling.
+            $('.completed-checkbox').change(function (event) {
 
-        // Note completed checkbox event handling.
-        $('.completed-checkbox').change(function (event) {
+                // Update the note's completed property and store
+                // it in the note storage.
+                var checkBox = $(event.currentTarget)[0];
+                var noteElement = $(checkBox).parents('.note')[0];
+                var id = Number($(noteElement).attr('id'));
+                noteStorage.getNote(id, function(note) {
+                    note.completed = checkBox.checked;
+                    noteStorage.updateNote(note, function(note) {});
 
-            // Update the note's completed property and store
-            // it in the note storage.
-            var checkBox = $(event.currentTarget)[0];
-            var noteElement = $(checkBox).parents('.note')[0];
-            var id = Number($(noteElement).attr('id'));
-            noteStorage.getNote(id, function(note) {
-                note.completed = checkBox.checked;
-                noteStorage.updateNote(note, function(note) {});
-
+                });
             });
+            template.afterRender();
         });
-    });
-
-}
+    }
+};
 
 // execute on load
 var noteListTemplate = Handlebars.compile(document.getElementById('noteListTemplate').innerHTML);
-
+var indexHeaderTemplate = Handlebars.compile(document.getElementById('indexHeader').innerHTML);
 
 function orderAndFilterNotes(notes, settings) {
 
@@ -55,24 +59,14 @@ function getMainElement() {
     return document.getElementsByTagName('main')[0];
 }
 
-document.getElementById("new-note").onclick = function() {
-    window.location.replace('editNote.html')
-};
+
 
 function setNotesOrderBy(orderBy) {
     var settings = noteSettings.readSettings();
     settings.orderBy = orderBy;
     noteSettings.updateSettings(settings);
-    renderList();
+    template.render();
 }
-
-document.getElementById("order-by-due").onclick = function() {
-    setNotesOrderBy(noteSettings.Settings.orderByDue);
-};
-
-document.getElementById("order-by-importance").onclick = function() {
-    setNotesOrderBy(noteSettings.Settings.orderByImportance);
-};
 
 function initializeCompletedFilter() {
     var settings = noteSettings.readSettings();
@@ -83,11 +77,23 @@ function initializeCompletedFilter() {
         var settings = noteSettings.readSettings();
         settings.excludeCompletedNotes = checkBox.checked;
         noteSettings.updateSettings(settings);
-        renderList();
+        template.render();
     });
 }
 
 // execute on load
-initializeCompletedFilter();
-renderList();
+template.render();
+template.afterRender = function() { //callback function in renderPAge
+    initializeCompletedFilter();
 
+    document.getElementById("new-note").onclick = function () {
+        window.location.replace('editNote.html')
+    };
+    document.getElementById("order-by-due").onclick = function () {
+        setNotesOrderBy(noteSettings.Settings.orderByDue);
+    };
+
+    document.getElementById("order-by-importance").onclick = function () {
+        setNotesOrderBy(noteSettings.Settings.orderByImportance);
+    };
+};
