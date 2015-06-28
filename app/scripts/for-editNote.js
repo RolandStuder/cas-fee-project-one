db0bee556808f91b0d1f44bbcbbb1427f3431b0d
Roland Studer <roland.studer@gmail.com>
2015-06-28 11:14:53 +0200

    wrapped in IIFE to not pollute global namespace

--- app/scripts/for-editNote.js
+++ app/scripts/for-editNote.js
@@ -2,227 +2,231 @@
  * Created by Luzius on 22.05.2015.
  */
 
-/**
- * Sets the note input fields from a Note instance.
- *
- * @param {Note} note The source note.
- */
-function displayNoteDataInForm(note) {
 
-    $(titleElement()).val(note.title);
-    $(descriptionElement()).val(note.description);
+(function(){
+    /**
+     * Sets the note input fields from a Note instance.
+     *
+     * @param {Note} note The source note.
+     */
+    function displayNoteDataInForm(note) {
 
-    if(hasInputType("date")) {
-        // Chrome, format the date as YYYY-MM-DD
-        $(dueElement()).val(
-            note.due.getFullYear() + "-" +
-            utilities.padLeft(String((note.due.getMonth() + 1)), 2, "0") + "-" +
-            utilities.padLeft(String(note.due.getDate()), 2, "0"));
+        $(titleElement()).val(note.title);
+        $(descriptionElement()).val(note.description);
+
+        if(hasInputType("date")) {
+            // Chrome, format the date as YYYY-MM-DD
+            $(dueElement()).val(
+                note.due.getFullYear() + "-" +
+                utilities.padLeft(String((note.due.getMonth() + 1)), 2, "0") + "-" +
+                utilities.padLeft(String(note.due.getDate()), 2, "0"));
+        }
+        else {
+            // Date is not supported, format the date as DD.MM.YYYY
+            $(dueElement()).val(
+                utilities.padLeft(String(note.due.getDate()), 2, "0") + "." +
+                utilities.padLeft(String((note.due.getMonth() + 1)), 2, "0") + "." +
+                note.due.getFullYear());
+        }
+
+        $('#importance-' + note.importance).prop('checked', true);
     }
-    else {
-        // Date is not supported, format the date as DD.MM.YYYY
-        $(dueElement()).val(
-            utilities.padLeft(String(note.due.getDate()), 2, "0") + "." +
-            utilities.padLeft(String((note.due.getMonth() + 1)), 2, "0") + "." +
-            note.due.getFullYear());
+
+    /**
+     * Checks if the browser supports an input type.
+     *
+     * @param type {string}
+     * @returns {boolean} true is the input type is supported, false if not.
+     */
+    function hasInputType(type) {
+        var input = document.createElement("input");
+        input.setAttribute("type", type);
+        return input.type == type;
     }
 
-    $('#importance-' + note.importance).prop('checked', true);
-}
 
-/**
- * Checks if the browser supports an input type.
- *
- * @param type {string}
- * @returns {boolean} true is the input type is supported, false if not.
- */
-function hasInputType(type) {
-    var input = document.createElement("input");
-    input.setAttribute("type", type);
-    return input.type == type;
-}
+    /**
+     * Stores the note input fields in a Note instance.
+     *
+     * @param {Note} note - The destination note.
+     */
+    function getNote(note) {
+        note.title = $(titleElement()).val();
+        note.description = $(descriptionElement()).val();
+        note.due = parseDateString($(dueElement()).val());
+        note.importance = Number($('input[name="importance"]:checked').val());
+    }
 
-
-/**
- * Stores the note input fields in a Note instance.
- *
- * @param {Note} note - The destination note.
- */
-function getNote(note) {
-    note.title = $(titleElement()).val();
-    note.description = $(descriptionElement()).val();
-    note.due = parseDateString($(dueElement()).val());
-    note.importance = Number($('input[name="importance"]:checked').val());
-}
-
-function titleElement() {
-    return $("#title");
-}
-function descriptionElement() {
-    return $("#note-text");
-}
-function dueElement() {
-    return $("#due");
-}
+    function titleElement() {
+        return $("#title");
+    }
+    function descriptionElement() {
+        return $("#note-text");
+    }
+    function dueElement() {
+        return $("#due");
+    }
 
 
 
-/**
- * Parses a date string of format YYYY-MM-DD or DD.MM.YYYY
- *
- * @param {string} dateString The string to parse.
- * @returns {Date} The parsed date or null if the date string is invalid.
- */
-function parseDateString(dateString) {
+    /**
+     * Parses a date string of format YYYY-MM-DD or DD.MM.YYYY
+     *
+     * @param {string} dateString The string to parse.
+     * @returns {Date} The parsed date or null if the date string is invalid.
+     */
+    function parseDateString(dateString) {
 
-    // This works if dateString is YYYY-MM-DD (Chrome!)
-    var date = new Date(dateString);
+        // This works if dateString is YYYY-MM-DD (Chrome!)
+        var date = new Date(dateString);
 
-    if (isNaN(date.getTime())) {
-        // Browser does not know the date tag => parse the input manually.
-        // Expected format: DD.MM.YYYY
-        var dateElements = dateString.split('.');
-        if (dateElements.length === 3) {
-            date = new Date();
-            date.setFullYear(dateElements[2]);
-            date.setMonth(dateElements[1] - 1);
-            date.setDate(dateElements[0]);
-            if(date.getFullYear() != dateElements[2] || date.getMonth() != dateElements[1] -1 || date.getDate() != dateElements[0])
-            {
+        if (isNaN(date.getTime())) {
+            // Browser does not know the date tag => parse the input manually.
+            // Expected format: DD.MM.YYYY
+            var dateElements = dateString.split('.');
+            if (dateElements.length === 3) {
+                date = new Date();
+                date.setFullYear(dateElements[2]);
+                date.setMonth(dateElements[1] - 1);
+                date.setDate(dateElements[0]);
+                if(date.getFullYear() != dateElements[2] || date.getMonth() != dateElements[1] -1 || date.getDate() != dateElements[0])
+                {
+                    date = null;
+                }
+            }
+            else {
                 date = null;
             }
         }
-        else {
-            date = null;
+        return date;
+    }
+
+    /**
+     * Validates the inputs.
+     *
+     * @returns {boolean} true if valid, false if invalid.
+     */
+    function validate() {
+        if ($(titleElement()).val() === "") {
+            $('#title-error').html("Titel muss eingegeben werden");
+            titleElement().focus();
+            return false;
         }
-    }
-    return date;
-}
 
-/**
- * Validates the inputs.
- *
- * @returns {boolean} true if valid, false if invalid.
- */
-function validate() {
-    if ($(titleElement()).val() === "") {
-        $('#title-error').html("Titel muss eingegeben werden");
-        titleElement().focus();
-        return false;
+        var dateString = $(dueElement()).val();
+        if (parseDateString(dateString) == null) {
+            $('#date-error').html("Ungültiges Datum. Erwartetes Format: dd.mm.jjjj, z.B. 23.11.2014");
+            dueElement().val("dd.mm.jjjj");
+            dueElement().focus();
+            return false;
+        }
+
+        return true;
+
     }
 
-    var dateString = $(dueElement()).val();
-    if (parseDateString(dateString) == null) {
-        $('#date-error').html("Ungültiges Datum. Erwartetes Format: dd.mm.jjjj, z.B. 23.11.2014");
-        dueElement().val("dd.mm.jjjj");
-        dueElement().focus();
-        return false;
-    }
 
-    return true;
-
-}
+    /**
+     * Page initialization.
+     */
+    function initialize() {
 
 
-/**
- * Page initialization.
- */
-function initialize() {
+        // Command intialization (uses handlebar).
 
+        function initializeCommands() {
 
-    // Command intialization (uses handlebar).
+            // The available commands.
+            var commands = [
+                {'commandId' : 'save', type: 'submit', 'caption' : 'Speichern', click : save, class: 'primary-action'},
+                {'commandId' : 'cancel', type: 'button', 'caption' : 'Abbruch', click : backToStartPage, class: 'secondary-action'}
+            ];
 
-    function initializeCommands() {
+            // Set the commands html with handle bar.
+            var commandsTemplateText = $('#commands-template').html();
+            var createCommandsHtml = Handlebars.compile(commandsTemplateText);
+            $('.commands').html(createCommandsHtml(commands));
 
-        // The available commands.
-        var commands = [
-            {'commandId' : 'save', type: 'submit', 'caption' : 'Speichern', click : save, class: 'primary-action'},
-            {'commandId' : 'cancel', type: 'button', 'caption' : 'Abbruch', click : backToStartPage, class: 'secondary-action'}
-        ];
+            // Assign the event handlers.
+            commands.forEach(function(command) {
+                $('#' + command.commandId).on('click', command.click);
+            });
+            $('form').submit(function(){save()});
+        }
 
-        // Set the commands html with handle bar.
-        var commandsTemplateText = $('#commands-template').html();
-        var createCommandsHtml = Handlebars.compile(commandsTemplateText);
-        $('.commands').html(createCommandsHtml(commands));
-
-        // Assign the event handlers.
-        commands.forEach(function(command) {
-            $('#' + command.commandId).on('click', command.click);
-        });
-        $('form').submit(function(){save()});
-    }
-
-    function enableFloatingLabels() {
-        var targets = $($('input').add('textarea'));
-        targets.each(function(){
-            if (this.value.length > 0)
+        function enableFloatingLabels() {
+            var targets = $($('input').add('textarea'));
+            targets.each(function(){
+                if (this.value.length > 0)
+                    $('[for='+this.id+']').addClass('is-floated');
+            });
+            targets.on('focus', function(){
                 $('[for='+this.id+']').addClass('is-floated');
+            });
+            targets.on('blur', function() {
+                if (this.value.length === 0) $('[for='+this.id+']').removeClass('is-floated');
+            });
+        }
+
+
+        var noteStorage = noteData.noteStorageSingleton.getInstance();
+
+        var parameters = utilities.getParametersFromQueryString(window.location.search);
+
+        var note;
+
+        function noteAvailable(noteFromStorage) {
+            note = noteFromStorage;
+            displayNoteDataInForm(note);
+            enableFloatingLabels();
+        }
+
+        if ('id' in parameters) {
+            noteStorage.getNote(Number(parameters.id), noteAvailable);
+        }
+        else {
+            noteStorage.newNote(noteAvailable);
+        }
+
+        appSettings.initializeSettings(function(){
+            themeSwitcher.loadTheme('style2');
+            $('.header-layout-switcher').on('click',function(){
+                themeSwitcher.toggleTheme()
+            });
         });
-        targets.on('focus', function(){
-            $('[for='+this.id+']').addClass('is-floated');
-        });
-        targets.on('blur', function() {
-            if (this.value.length === 0) $('[for='+this.id+']').removeClass('is-floated');
-        });
-    }
+        initializeCommands();
 
+        // Button event handlers.
+        function backToStartPage() {
+            window.location.replace('index.html');
+        }
 
-    var noteStorage = noteData.noteStorageSingleton.getInstance();
-
-    var parameters = utilities.getParametersFromQueryString(window.location.search);
-
-    var note;
-
-    function noteAvailable(noteFromStorage) {
-        note = noteFromStorage;
-        displayNoteDataInForm(note);
-        enableFloatingLabels();
-    }
-
-    if ('id' in parameters) {
-        noteStorage.getNote(Number(parameters.id), noteAvailable);
-    }
-    else {
-        noteStorage.newNote(noteAvailable);
-    }
-
-    appSettings.initializeSettings(function(){
-        themeSwitcher.loadTheme('style2');
-        $('.header-layout-switcher').on('click',function(){
-            themeSwitcher.toggleTheme()
-        });
-    });
-    initializeCommands();
-
-    // Button event handlers.
-    function backToStartPage() {
-        window.location.replace('index.html');
-    }
-
-    function save (event) {
-        event.preventDefault();
-        if (validate()) {
-            getNote(note);
-            noteStorage.updateNote(note, function(note){});
-            backToStartPage();
+        function save (event) {
+            event.preventDefault();
+            if (validate()) {
+                getNote(note);
+                noteStorage.updateNote(note, function(note){});
+                backToStartPage();
+            }
         }
     }
-}
 
-$(function () {
-        try {
+    $(function () {
+            try {
 
-            // Alert ajax errors.
-            utilities.alertAjaxErrors();
+                // Alert ajax errors.
+                utilities.alertAjaxErrors();
 
-            // Initialize page.
-            initialize();
+                // Initialize page.
+                initialize();
 
 
+            }
+            catch (exception) {
+                console.error(exception);
+                alert('Es ist ein Fehler aufgetreten:\n' + exception.toString());
+            }
         }
-        catch (exception) {
-            console.error(exception);
-            alert('Es ist ein Fehler aufgetreten:\n' + exception.toString());
-        }
-    }
-);
+    );
+
+})();
--- app/scripts/for-index.js
+++ app/scripts/for-index.js
@@ -1,172 +1,175 @@
-var template = {
-    render: function () {
-        var settings = appSettings.getSettings();
-        var noteStorage = noteData.noteStorageSingleton.getInstance();
-        noteStorage.readNotes(function (notes) {
-            notes = orderAndFilterNotes(notes, settings);
-            $('header').empty().html(indexHeaderTemplate(settings));
-            $(getMainElement()).empty().html(noteListTemplate({notes: notes, settings: settings}));
-            template.afterRender();
-        });
-    }
-};
-
-function orderAndFilterNotes(notes, settings) {
-
-    //  alert(notes[2].completed)
-
-    if (settings.excludeCompletedNotes) {
-        notes = notes.filter(function (note) {
-            return !note.completed;
-        });
-    }
-    if (settings.orderBy === appSettings.Settings.orderByDue) {
-        notes.reverse(); // reversing array here to create consistency in display between duedate and creation date. Otherwise later created notes with same duedate are displayed reversed.
-        notes.sort(function (note1, note2) {
-            // Descending.
-            return note2.due - note1.due;
-        })
-    }
-    else if (settings.orderBy === appSettings.Settings.orderByCreationDate) {
-        notes.sort(function (note1, note2) {
-            // Descending.
-            return note2.creationDate - note1.creationDate;
-        })
-    }
-    else if (settings.orderBy === appSettings.Settings.orderByImportance) {
-        // Descending.
-        notes.sort(function (note1, note2) {
-            return note2.importance - note1.importance;
-        })
-    }
-    return notes;
-}
-
-function getMainElement() {
-    return document.getElementsByTagName('main')[0];
-}
-
-
-function setNotesOrderBy(orderBy) {
-    var settings = appSettings.getSettings();
-    settings.orderBy = orderBy;
-    appSettings.updateSettings();
-    template.render();
-}
-
-function toggleExcludeCompletedNotes(){
-    var settings = appSettings.getSettings();
-    settings.excludeCompletedNotes = !settings.excludeCompletedNotes;
-    appSettings.updateSettings();
-    template.render();
-}
-
-var noteListTemplate;
-var indexHeaderTemplate;
-
-/**
- * Initializes Handelbars stuff.
- */
-function initializeHandleBars() {
-
-    /**
-     *  The date formatter.
-     */
-    Handlebars.registerHelper("formatDate", function (date) {
-        return date.getDate() + "." + (date.getMonth() + 1) + "." + date.getFullYear();
-    });
-
-    /**
-     *  The importance formatter. Converts the importance number to stars.
-     */
-    Handlebars.registerHelper("formatImportance", function (importance) {
-        return new Handlebars.SafeString(Array(importance + 1).join('<i class="fa fa-star"></i>'));
-    });
-
-    var isActive = 'is-active';
-    var isNotActive = 'is-not-active';
-
-    function getIsActiveClass(orderBy) {
-        var settings = appSettings.getSettings();
-        if(settings.orderBy == orderBy){
-            return isActive;
-        }
-        else {
-            return isNotActive;
-        }
-    }
-
-    /**
-     * Returns the is-active / is-not-active class of the due order by button.
-     */
-    Handlebars.registerHelper("orderByDueIsActive", function () {
-        return getIsActiveClass(appSettings.Settings.orderByDue)
-    });
-
-    /**
-     * Returns the is-active / is-not-active class of the importance order by button.
-     */
-    Handlebars.registerHelper("orderByImportanceIsActive", function () {
-        return getIsActiveClass(appSettings.Settings.orderByImportance)
-    });
-
-    /**
-     * Returns the is-active / is-not-active class of the creation date order by button.
-     */
-    Handlebars.registerHelper("orderByCreationDateIsActive", function () {
-        return getIsActiveClass(appSettings.Settings.orderByCreationDate)
-    });
-
-    noteListTemplate = Handlebars.compile(document.getElementById('noteListTemplate').innerHTML);
-    indexHeaderTemplate = Handlebars.compile(document.getElementById('indexHeader').innerHTML);
-
-}
-
-function attachEventHandlers(){
-
-    var noteStorage = noteData.noteStorageSingleton.getInstance();
-
-    $("#new-note").on('click',function () {
-        window.location.replace('editNote.html')
-    });
-
-    $("[data-action='order']").on('click', function(){
-        var orderBy = $(this).attr('data-order-by');
-        setNotesOrderBy(orderBy);
-    });
-
-    $("[data-action='exclude-completed-notes']").on('click', function(){
-        toggleExcludeCompletedNotes();
-    });
-
-    $('.note-completion').on('click', function (event) {
-        // Update the note's completed property and store
-        // it in the note storage.
-        var noteElement = $(this).parents('.note')[0];
-        var id = Number($(noteElement).attr('id'));
-        noteStorage.getNote(id, function (note) {
-            note.completed = !note.completed;
-            noteStorage.updateNote(note, function (note) {
+(function(){
+    var template = {
+        render: function () {
+            var settings = appSettings.getSettings();
+            var noteStorage = noteData.noteStorageSingleton.getInstance();
+            noteStorage.readNotes(function (notes) {
+                notes = orderAndFilterNotes(notes, settings);
+                $('header').empty().html(indexHeaderTemplate(settings));
+                $(getMainElement()).empty().html(noteListTemplate({notes: notes, settings: settings}));
+                template.afterRender();
             });
-            template.render();
-        });
-    });
+        }
+    };
 
 
-    $('.header-layout-switcher').on('click',  function() {
-        themeSwitcher.toggleTheme();
-    });
-}
+    function orderAndFilterNotes(notes, settings) {
 
-// execute on load
-$(function () {
-    utilities.alertAjaxErrors();
-    appSettings.initializeSettings(function () {
-        themeSwitcher.loadTheme();
-        initializeHandleBars();
+        //  alert(notes[2].completed)
+
+        if (settings.excludeCompletedNotes) {
+            notes = notes.filter(function (note) {
+                return !note.completed;
+            });
+        }
+        if (settings.orderBy === appSettings.Settings.orderByDue) {
+            notes.reverse(); // reversing array here to create consistency in display between duedate and creation date. Otherwise later created notes with same duedate are displayed reversed.
+            notes.sort(function (note1, note2) {
+                // Descending.
+                return note2.due - note1.due;
+            })
+        }
+        else if (settings.orderBy === appSettings.Settings.orderByCreationDate) {
+            notes.sort(function (note1, note2) {
+                // Descending.
+                return note2.creationDate - note1.creationDate;
+            })
+        }
+        else if (settings.orderBy === appSettings.Settings.orderByImportance) {
+            // Descending.
+            notes.sort(function (note1, note2) {
+                return note2.importance - note1.importance;
+            })
+        }
+        return notes;
+    }
+
+    function getMainElement() {
+        return document.getElementsByTagName('main')[0];
+    }
+
+
+    function setNotesOrderBy(orderBy) {
+        var settings = appSettings.getSettings();
+        settings.orderBy = orderBy;
+        appSettings.updateSettings();
         template.render();
-        template.afterRender = function () { //callback function in renderPage
-            attachEventHandlers();
-        };
-    })
-});
+    }
+
+    function toggleExcludeCompletedNotes(){
+        var settings = appSettings.getSettings();
+        settings.excludeCompletedNotes = !settings.excludeCompletedNotes;
+        appSettings.updateSettings();
+        template.render();
+    }
+
+    var noteListTemplate;
+    var indexHeaderTemplate;
+
+    /**
+     * Initializes Handelbars stuff.
+     */
+    function initializeHandleBars() {
+
+        /**
+         *  The date formatter.
+         */
+        Handlebars.registerHelper("formatDate", function (date) {
+            return date.getDate() + "." + (date.getMonth() + 1) + "." + date.getFullYear();
+        });
+
+        /**
+         *  The importance formatter. Converts the importance number to stars.
+         */
+        Handlebars.registerHelper("formatImportance", function (importance) {
+            return new Handlebars.SafeString(Array(importance + 1).join('<i class="fa fa-star"></i>'));
+        });
+
+        var isActive = 'is-active';
+        var isNotActive = 'is-not-active';
+
+        function getIsActiveClass(orderBy) {
+            var settings = appSettings.getSettings();
+            if(settings.orderBy == orderBy){
+                return isActive;
+            }
+            else {
+                return isNotActive;
+            }
+        }
+
+        /**
+         * Returns the is-active / is-not-active class of the due order by button.
+         */
+        Handlebars.registerHelper("orderByDueIsActive", function () {
+            return getIsActiveClass(appSettings.Settings.orderByDue)
+        });
+
+        /**
+         * Returns the is-active / is-not-active class of the importance order by button.
+         */
+        Handlebars.registerHelper("orderByImportanceIsActive", function () {
+            return getIsActiveClass(appSettings.Settings.orderByImportance)
+        });
+
+        /**
+         * Returns the is-active / is-not-active class of the creation date order by button.
+         */
+        Handlebars.registerHelper("orderByCreationDateIsActive", function () {
+            return getIsActiveClass(appSettings.Settings.orderByCreationDate)
+        });
+
+        noteListTemplate = Handlebars.compile(document.getElementById('noteListTemplate').innerHTML);
+        indexHeaderTemplate = Handlebars.compile(document.getElementById('indexHeader').innerHTML);
+
+    }
+
+    function attachEventHandlers(){
+
+        var noteStorage = noteData.noteStorageSingleton.getInstance();
+
+        $("#new-note").on('click',function () {
+            window.location.replace('editNote.html')
+        });
+
+        $("[data-action='order']").on('click', function(){
+            var orderBy = $(this).attr('data-order-by');
+            setNotesOrderBy(orderBy);
+        });
+
+        $("[data-action='exclude-completed-notes']").on('click', function(){
+            toggleExcludeCompletedNotes();
+        });
+
+        $('.note-completion').on('click', function (event) {
+            // Update the note's completed property and store
+            // it in the note storage.
+            var noteElement = $(this).parents('.note')[0];
+            var id = Number($(noteElement).attr('id'));
+            noteStorage.getNote(id, function (note) {
+                note.completed = !note.completed;
+                noteStorage.updateNote(note, function (note) {
+                });
+                template.render();
+            });
+        });
+
+
+        $('.header-layout-switcher').on('click',  function() {
+            themeSwitcher.toggleTheme();
+        });
+    }
+
+    // execute on load
+    $(function () {
+        utilities.alertAjaxErrors();
+        appSettings.initializeSettings(function () {
+            themeSwitcher.loadTheme();
+            initializeHandleBars();
+            template.render();
+            template.afterRender = function () { //callback function in renderPage
+                attachEventHandlers();
+            };
+        })
+    });
+})();
